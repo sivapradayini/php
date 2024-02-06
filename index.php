082111e6c94d24cc9d7ba740c78c87d4c16d72a5
Sivapradayini Neelappan <63453793+sivapradayini@users.noreply.github.com>
2024-02-06 11:34:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,22 +23,27 @@
 		
 		// Taking all 5 values from the form data(input)
 //name
+// $name = $_REQUEST['name'];
 		$first_name = $_REQUEST['first_name'];
 		$last_name = $_REQUEST['last_name'];
 
-//Mobile - 10 digits validation eg 9791952564
+//Mobile - 10 digits validation eg 9791952564 in html
+// $mobile = $_REQUEST['mobile'];
 		$gender = $_REQUEST['gender'];
-//E-Mail - check for @ symbol validation
+//E-Mail - check for @ symbol validation using javascript or html
+// $mail = $_REQUEST['email'];
 		
 		$email = $_REQUEST['email'];
 //Message - No Validation Long text variable
+// $message = $_REQUEST['message'];
+
 $address = $_REQUEST['address'];
 		
 		// Performing insert query execution
 		// here our table name is college
 		$sql = "INSERT INTO college VALUES ('$first_name', 
 			'$last_name','$gender','$address','$email')";
-		
+		// $sql = "INSERT INTO siva VALUES ('$name','$mobile','$email','$message')";
 		if(mysqli_query($conn, $sql)){
 			echo "<h3>data stored in a database successfully."
 				. " Please browse your localhost php my admin"
@@ -46,6 +51,9 @@ $address = $_REQUEST['address'];
 
 			echo nl2br("\n$first_name\n $last_name\n "
 				. "$gender\n $address\n $email");
+
+			// echo nl2br("\n$name\n"
+				. "$mobile\n $email\n $message");
 		} else{
 			echo "ERROR: Hush! Sorry $sql. "
 				. mysqli_error($conn);
